c06cea26d6f643774137e096a53581657f377452
Dimitris Angelakis <agelakis@gmail.com>
2024-06-17 16:14:27 +0300

    testing custom footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -49,7 +49,84 @@ class SiteFooter extends React.Component {
         className="footer d-flex border-top py-3 px-4"
       >
         <div className="container-fluid d-flex">
-          <span>TESTING FOOTER</span>
+          <span>
+
+
+
+
+
+
+
+
+
+
+
+
+          <nav class="nav-inline">
+        <ol>
+          <span><a href="/foreas">ΠΕΡΙ</a></span>
+          <span><a href="/news">ΝΕΑ</a></span>
+          <span><a href="/faq">ΣΥΧΝΕΣ ΕΡΩΤΗΣΕΙΣ</a></span>
+          <span><a href="/honor">ΟΡΟΙ ΧΡΗΣΗΣ</a></span>
+          <span><a href="/privacy">ΠΡΟΣΩΠΙΚΑ ΔΕΔΟΜΕΝΑ</a></span>
+          <span><a href="/contact">ΕΠΙΚΟΙΝΩΝΙΑ</a></span>
+          <span><a href="https://pay.mathesis.org/el/payments/donate/">ΔΩΡΕΕΣ</a></span>
+        </ol>
+      </nav>
+
+<div class="leftcol">
+<font size="" style="color:#575757">© Mathesis</font>
+<a target="_blank" href="http://open.edx.org"><img alt="Βασισμένο στο Open edX" src="/static/EuCup/images/openedx.png" width="100" style="margin-top: -6px;" title=""></a>
+
+</div>
+
+
+<div class="rightcol">
+<a target="_blank" href="https://www.facebook.com/mathesisatcreteuniversitypress" class="share"><i class="icon fa fa-facebook-square"></i></a>
+<a target="_blank" href="https://www.instagram.com/mathesis_cup/" class="share"><i class="icon fa fa-instagram"></i></a>
+<a target="_blank" href="https://twitter.com/mathesiscup" class="share"><i class="icon fa fa-twitter-square"></i></a>
+<a target="_blank" href="https://www.linkedin.com/company/mathesis-crete-university-press" class="share"> <i class="icon fa fa-linkedin-square"></i></a>
+</div>
+<div class="midcol">
+
+<table style="margin-right:auto;margin-left:auto;"><tbody><tr>
+<td style="vertical-align:middle;padding-right:20px;">
+	<a target="_blank" href="http://www.cup.gr">
+<img src="/static/EuCup/images/cupfl_o.svg" height="100px" />
+</a></td>
+
+<td style="vertical-align:middle; text-align: center;"><font size="-1.2">Με δωρεά από το</font><br><a target="_blank" href="http://www.snf.org/el/">
+
+<img src="/c4x/edX/DemoX/asset/SNFv_o.svg" alt="ΙΔΡΥΜΑ ΣΤΑΥΡΟΣ ΝΙΑΡΧΟΣ" height="70px" />
+</a></td>
+	<td style="text-align:center; font-size:1.9em;width: 100px;">
+
+</td></tr>
+</tbody></table>
+</div>
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          </span>
           <a
             className="d-block"
             href={config.LMS_BASE_URL}
